c75dd3d22802a99c7b635505775e4533dfcae81c
DJ Jaiswal <digvijays18@gmail.com>
2016-09-28 04:24:27 +0000

    Done.

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ function nestedTarget(){
   return document.querySelector('.target')
 }
 
-
+var incRank = document.querySelectorAll('.ranked-list li')
 function increaseRankBy(n){
-  var incRank = document.querySelectorAll('.ranked-list li')
-  var a = [];
     for (var i=0; i<incRank.length; i++){
-      a.push(parseInt(incRank[i].innerHTML) + n)
-    } return a
+      incRank[i].innerHTML = (parseInt(incRank[i].innerHTML) + n)
+    }
   }
+// var deepDiv = document.querySelector('#grand-node div div div div').innerHTML
+var topDiv = document.querySelectorAll('#grand-node')
+function deepestChild(){
+  var topDiv = document.querySelectorAll('#grand-node')
+    for(var i=0; i<topDiv.length; i++){
+      var deepDiv = topDiv[i]
+      return deepDiv
+    }
+}
